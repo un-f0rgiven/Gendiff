bd168752cbb66d6f294ffa3ce3ecb0c19c0263ec
un-f0rgiven <barr85@mail.ru>
2024-09-19 14:00:09 +1000

    update file

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -9,48 +9,43 @@ def format_value(value):
     return value
 
 
-def generate_diff(first_file, second_file):
-    first_ext = os.path.splitext(first_file)[1]
-    second_ext = os.path.splitext(second_file)[1]
-
-    if first_ext == '.json':
-        first_data = parse_json_file(first_file)
+def get_file_data(file_path):
+    ext = os.path.splitext(file_path)[1]
+    if ext == '.json':
+        return parse_json_file(file_path)
     else:
-        first_data = parse_yaml_file(first_file)
+        return parse_yaml_file(file_path)
 
-    if second_ext == '.json':
-        second_data = parse_json_file(second_file)
-    else:
-        second_data = parse_yaml_file(second_file)
 
-    # Объединяем ключи из обоих файлов и сортируем их в алфавитном порядке
-    all_keys = sorted(first_data.keys() | second_data.keys())
-
+def format_diff_lines(all_keys, first_data, second_data):
     diff_lines = []
-
     for key in all_keys:
         first_value = first_data.get(key)
         second_value = second_data.get(key)
 
-        # Применяем форматирование для значений
         formatted_first_value = format_value(first_value)
         formatted_second_value = format_value(second_value)
 
-        # Если значение отсутствует в первом файле
         if key not in first_data:
             diff_lines.append(f"+ {key}: {formatted_second_value}")
-        # Если значение отсутствует во втором файле
         elif key not in second_data:
             diff_lines.append(f"- {key}: {formatted_first_value}")
-        # Если значения различаются
         elif formatted_first_value != formatted_second_value:
             diff_lines.append(f"- {key}: {formatted_first_value}")
             diff_lines.append(f"+ {key}: {formatted_second_value}")
-        # Если значения совпадают
         else:
             diff_lines.append(f"  {key}: {formatted_first_value}")
 
-    # Собираем результат в фигурные скобки
+    return diff_lines
+
+
+def generate_diff(first_file, second_file):
+    first_data = get_file_data(first_file)
+    second_data = get_file_data(second_file)
+
+    all_keys = sorted(first_data.keys() | second_data.keys())
+    diff_lines = format_diff_lines(all_keys, first_data, second_data)
+
     result = "{\n" + "\n".join(diff_lines) + "\n}"
     return result
 
