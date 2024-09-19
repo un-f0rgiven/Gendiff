d3e7ea6bfccc6dee80c5e2e4b06bc2efcdd0485c
un-f0rgiven <barr85@mail.ru>
2024-09-19 18:08:05 +1000

    update file

--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -1,49 +1,99 @@
-def format_value(value):
-    if isinstance(value, dict):
-        return format_dict(value)
-    if isinstance(value, list):
-        return format_list(value)
-    if value is None:
-        return 'null'
-    return str(value).lower() if isinstance(value, bool) else str(value)
-
-def format_dict(data):
-    lines = []
-    for key, item in sorted(data.items()):
-        lines.append(f"{key}: {format_value(item)}")
-    return "{\n" + "\n".join(lines) + "\n}"
-
-def format_list(data):
-    return "[\n" + ",\n".join(f"  {format_value(item)}" for item in data) + "\n]"
+from vision import build_diff
 
 def format_diff(diff, indent=0):
     result = []
-    indent_space = ' ' * indent
-
-    for key, change in sorted(diff.items()):
-        status = change['status']
-
+    for node in diff:
+        key = node['key']
+        status = node['status']
+        value = node.get('value')
+        indentation = ' ' * indent
+        
         if status == 'added':
-            result.append(f"{indent_space}+ {key}: {format_value(change['value'])}")
+            result.append(f"{indentation}+ {key}: {value}" if not isinstance(value, dict) else f"{indentation}+ {key}: {{")
+            if isinstance(value, dict):
+                result.extend(format_diff(build_diff({}, value), indent + 2))
+                result.append(f"{indentation}}}")
         elif status == 'removed':
-            result.append(f"{indent_space}- {key}: {format_value(change['value'])}")
+            result.append(f"{indentation}- {key}: {value}" if not isinstance(value, dict) else f"{indentation}- {key}: {{")
+            if isinstance(value, dict):
+                result.extend(format_diff(build_diff(value, {}), indent + 2))
+                result.append(f"{indentation}}}")
         elif status == 'modified':
-            old_value, new_value = change['value'] if change['value'] else (None, None)
-            if old_value is not None:
-                result.append(f"{indent_space}- {key}: {format_value(old_value)}")
-            if new_value is not None:
-                result.append(f"{indent_space}+ {key}: {format_value(new_value)}")
-        elif status == 'unchanged':
-            result.append(f"{indent_space}  {key}: {format_value(change['value'])}")
+            result.append(f"{indentation}  {key}: {{")
+            result.extend(format_diff(node['children'], indent + 2))
+            result.append(f"{indentation}}}")
+        else:  # unchanged
+            result.append(f"{indentation}  {key}: {value}")
 
-        # Если есть дочерние элементы, форматируем их
-        if change.get('children'):
-            child_diff = {child['key']: child for child in change['children']}
-            child_result = format_diff(child_diff, indent + 2)
-            result.append(child_result)
+    return result
 
-    return "\n".join(result)
+first_data = {
+    "common": {
+        "setting1": "Value 1",
+        "setting2": 200,
+        "setting3": True,
+        "setting6": {
+            "key": "value",
+            "doge": {
+                "wow": ""
+            }
+        }
+    },
+    "group1": {
+        "baz": "bas",
+        "foo": "bar",
+        "nest": {
+            "key": "value"
+        }
+    },
+    "group2": {
+        "abc": 12345,
+        "deep": {
+            "id": 45
+        }
+    }
+}
 
-def format_full_diff(diff):
-    formatted_diff = format_diff(diff)
-    return "{\n" + formatted_diff + "\n}"
+second_data = {
+    "common": {
+        "follow": False,
+        "setting1": "Value 1",
+        "setting3": None,
+        "setting4": "blah blah",
+        "setting5": {
+            "key5": "value5"
+        },
+        "setting6": {
+            "key": "value",
+            "ops": "vops",
+            "doge": {
+                "wow": "so much"
+            }
+        }
+    },
+    "group1": {
+        "foo": "bar",
+        "baz": "bars",
+        "nest": "str"
+    },
+    "group3": {
+        "deep": {
+            "id": {
+                "number": 45
+            }
+        },
+        "fee": 100500
+    }
+}
+
+# Построение различий
+diff = build_diff(first_data, second_data)
+
+# print(diff)
+
+# Пример использования
+formatted_diff = format_diff(diff)
+
+print("{")
+print("\n".join(formatted_diff))
+print("}")
--- gendiff/vision.py
+++ gendiff/vision.py
@@ -1,4 +1,4 @@
-from stylish import format_full_diff
+
 
 def create_diff_node(key, status, value=None):
     return {
@@ -41,65 +41,3 @@ def build_diff(file1, file2):
 
     return diff  # Возвращаем список различий
 
-first_data = {
-    "common": {
-        "setting1": "Value 1",
-        "setting2": 200,
-        "setting3": True,
-        "setting6": {
-            "key": "value",
-            "doge": {
-                "wow": ""
-            }
-        }
-    },
-    "group1": {
-        "baz": "bas",
-        "foo": "bar",
-        "nest": {
-            "key": "value"
-        }
-    },
-    "group2": {
-        "abc": 12345,
-        "deep": {
-            "id": 45
-        }
-    }
-}
-
-second_data = {
-    "common": {
-        "follow": False,
-        "setting1": "Value 1",
-        "setting3": None,
-        "setting4": "blah blah",
-        "setting5": {
-            "key5": "value5"
-        },
-        "setting6": {
-            "key": "value",
-            "ops": "vops",
-            "doge": {
-                "wow": "so much"
-            }
-        }
-    },
-    "group1": {
-        "foo": "bar",
-        "baz": "bars",
-        "nest": "str"
-    },
-    "group3": {
-        "deep": {
-            "id": {
-                "number": 45
-            }
-        },
-        "fee": 100500
-    }
-}
-
-# Построение различий
-diff = build_diff(first_data, second_data)
-print(diff)
