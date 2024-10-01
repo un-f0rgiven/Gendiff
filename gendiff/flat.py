6e54e9e68091dcb41d60905f959ea9531afb06d1
un-f0rgiven <barr85@mail.ru>
2024-10-01 13:55:30 +1000

    add file

---
+++ gendiff/flat.py
@@ -0,0 +1,35 @@
+def format_value(value):
+    if isinstance(value, bool):
+        return str(value).lower()
+    return value
+
+
+def format_diff_lines(first_data, second_data):
+    diff_lines = []
+    all_keys = sorted(first_data.keys() | second_data.keys())
+
+    for key in all_keys:
+        first_value = first_data.get(key)
+        second_value = second_data.get(key)
+        formatted_first_value = format_value(first_value)
+        formatted_second_value = format_value(second_value)
+
+        if key not in first_data:
+            diff_lines.append(f"  + {key}: {formatted_second_value}")
+        elif key not in second_data:
+            diff_lines.append(f"  - {key}: {formatted_first_value}")
+        elif formatted_first_value != formatted_second_value:
+            diff_lines.append(f"  - {key}: {formatted_first_value}")
+            diff_lines.append(f"  + {key}: {formatted_second_value}")
+        else:
+            diff_lines.append(f"    {key}: {formatted_first_value}")
+
+    return diff_lines
+
+
+def print_diff_lines(first_data, second_data):
+    formatted_changes = format_diff_lines(first_data, second_data)
+    if formatted_changes:
+        results = "\n".join(formatted_changes)  # Создаем результирующую строку
+        return f"{{\n{results}\n}}"
+    return "{}"  # Возвращаем пустой словарь, если изменений нет
