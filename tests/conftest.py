582f447680bbbc241a7bef9e4adbd68449404254
un-f0rgiven <barr85@mail.ru>
2024-10-01 18:03:18 +1000

    update file

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import yaml
 import os
 
 @pytest.fixture
-def config_file1():
+def test_file1_json():
     """Загружает конфигурацию из test_file1.json."""
     config_file_path = os.path.join(os.path.dirname(__file__), 'fixtures', 'test_file1.json')
     with open(config_file_path, 'r') as f:
@@ -12,7 +12,7 @@ def config_file1():
     return data
 
 @pytest.fixture
-def config_file2():
+def test_file2_json():
     """Загружает конфигурацию из test_file2.json."""
     config_file_path = os.path.join(os.path.dirname(__file__), 'fixtures', 'test_file2.json')
     with open(config_file_path, 'r') as f:
@@ -20,17 +20,49 @@ def config_file2():
     return data
 
 @pytest.fixture
-def config_file_yaml1():
-    """Загружает конфигурацию из test_file.yaml."""
+def test_file1_yaml_():
+    """Загружает конфигурацию из test_file1.yaml."""
     config_file_path = os.path.join(os.path.dirname(__file__), 'fixtures', 'test_file1.yaml')
     with open(config_file_path, 'r') as f:
         data = yaml.safe_load(f)
     return data
 
 @pytest.fixture
-def config_file_yaml2():
-    """Загружает конфигурацию из test_file.yaml."""
+def test_file2_yaml():
+    """Загружает конфигурацию из test_file2.yaml."""
     config_file_path = os.path.join(os.path.dirname(__file__), 'fixtures', 'test_file2.yaml')
+    with open(config_file_path, 'r') as f:
+        data = yaml.safe_load(f)
+    return data
+
+@pytest.fixture
+def test_file1_r_json():
+    """Загружает конфигурацию из test_file1_r.json."""
+    config_file_path = os.path.join(os.path.dirname(__file__), 'fixtures', 'test_file1_r.json')
+    with open(config_file_path, 'r') as f:
+        data = json.load(f)
+    return data
+
+@pytest.fixture
+def test_file2_r_json():
+    """Загружает конфигурацию из test_file2_r.json."""
+    config_file_path = os.path.join(os.path.dirname(__file__), 'fixtures', 'test_file2_r.json')
+    with open(config_file_path, 'r') as f:
+        data = json.load(f)
+    return data
+
+@pytest.fixture
+def test_file1_r_yaml():
+    """Загружает конфигурацию из test_file1_r.yaml."""
+    config_file_path = os.path.join(os.path.dirname(__file__), 'fixtures', 'test_file1_r.yaml')
+    with open(config_file_path, 'r') as f:
+        data = yaml.safe_load(f)
+    return data
+
+@pytest.fixture
+def test_file2_r_yaml():
+    """Загружает конфигурацию из test_file2_r.yaml."""
+    config_file_path = os.path.join(os.path.dirname(__file__), 'fixtures', 'test_file2_r.yaml')
     with open(config_file_path, 'r') as f:
         data = yaml.safe_load(f)
     return data
